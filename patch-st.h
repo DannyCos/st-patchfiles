03bca042d0a5f2edcf9d8359fc8f77076952412a
DannyCos <DannyCos@users.noreply.github.com>
2024-10-25 13:05:39 +0100

    Added scrollback

--- patch-st.h
+++ patch-st.h
@@ -1,6 +1,15 @@
 --- st.h.orig	2022-10-04 17:41:26 UTC
 +++ st.h
-@@ -124,3 +124,4 @@ extern unsigned int defaultcs;
+@@ -81,6 +81,8 @@ void draw(void);
+ void redraw(void);
+ void draw(void);
+ 
++void kscrolldown(const Arg *);
++void kscrollup(const Arg *);
+ void printscreen(const Arg *);
+ void printsel(const Arg *);
+ void sendbreak(const Arg *);
+@@ -124,3 +126,4 @@ extern unsigned int defaultcs;
  extern unsigned int defaultfg;
  extern unsigned int defaultbg;
  extern unsigned int defaultcs;
